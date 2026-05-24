5b736815dd21d170629c09eeb3ba2ba6f325a294
RiCoWeSt2004 <riotgamesvalorantricowest@gmail.com>
2026-05-24 19:03:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 700,
+    title: "Mini Discord",
+    autoHideMenuBar: true, // Скрыть стандартное верхнее меню (Файл, Правка...)
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  // Загружаем наш HTML-файл
+  win.loadFile('index.html');
+}
+
+// Запрашиваем доступ к микрофону и экрану (для Electron это важно)
+app.commandLine.appendSwitch('enable-features', 'WebRTCPipeWireCapturer');
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
